93fc236bce6990fc428ffd5d3259a1630817295b
Roman <forthegranny@gmail.com>
2016-02-14 21:11:40 +0200

    Hello World

---
+++ src/snake/Startup.scala
@@ -0,0 +1,11 @@
+package snake
+
+/**
+  * Created by rteresch on 14.02.2016.
+  */
+object Startup {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+    scala.io.StdIn.readLine()
+  }
+}
